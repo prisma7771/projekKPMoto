bdf93e429d9be5f8d8332bd82c6155ac82cda349
prisma7771 <prismaputra91@gmail.com>
2022-05-30 19:53:15 +0700

    fix

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,12 +10,6 @@ class Post extends Model
 {
     use HasFactory, Sluggable;
 
-    // protected $fillable = [
-    //     'title',
-    //     'excerpt',
-    //     'body'
-    // ]; 
-
     protected $guarded = ['id'];
     protected $with = ['category','author'];
 
